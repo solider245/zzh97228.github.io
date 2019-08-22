5b27d475bb2ee9cac8848715988966ca975be007
zzh97228 <1002723879@qq.com>
2019-08-22 16:42:42 +0800

    init

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     description: 'kabama_house',
     // vuepress config
     dest: basePath,
-    base: `/${basePath}/`,
+    base: `https://zzh97228.github.io/${basePath}/`,
     serviceWorker: true,
     evergreen: true,
     ga: 'UA-112738831-1'
