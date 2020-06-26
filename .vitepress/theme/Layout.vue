9e90d71ca4a4eb0f48a9870419e4c2ce068b5bda
lagabu <1002723879@qq.com>
2020-06-26 22:41:37 +0800

    feat: update dependecy and make a homepage

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,16 +1,63 @@
 <template>
   <app>
-    <Content />
+    <nav-bar elevation="xs" height="48px">
+      <nav-bar-title>
+        <btn href="/index.html" depressed>
+          Lagabu's Blog
+        </btn>
+      </nav-bar-title>
+      <white-spacer></white-spacer>
+      <btn
+        nav
+        link
+        color="primary"
+        target="blank"
+        href="/README.html"
+        >README</btn
+      >
+      <btn
+        nav
+        link
+        color="primary"
+        target="blank"
+        href="https://github.com/zzh97228"
+        >Github</btn
+      >
+    </nav-bar>
+    <substance min-height="100vh">
+      <container>
+        <Content />
+      </container>
+    </substance>
+    <card color="primary" height="128px">
+
+    </card>
   </app>
 </template>
 <script>
-import { App } from 'lagabu/lib/entry-libs.js'
+import {
+  App,
+  Substance,
+  Container,
+  NavBar,
+  NavBarTitle,
+  WhiteSpacer,
+  Btn,
+  Card
+} from 'lagabu/lib/entry-libs.js';
 export default {
   name: 'layout',
   components: {
-    App
-  }  
-}
+    App,
+    Substance,
+    Container,
+    NavBar,
+    Btn,
+    NavBarTitle,
+    WhiteSpacer,
+    Card
+  },
+};
 </script>
 <style lang="less">
 @import './assets/main.less';
--- index.md
+++ index.md
@@ -1,3 +1,28 @@
-## Lagabu
+<h1 align="center">
+  Lagabu
+</h1>
 
-分享自己的见解和知识
+
+<card>
+  <card-title>
+    Lagabu
+  </card-title>
+  <card-content>
+    <btn color="primary">button</btn>
+  </card-content>
+</card>
+
+<script>
+import * as compos from 'lagabu/lib/components/index.js'
+export default {
+  name: 'temp',
+  components: {
+    ...compos
+  },
+  data() {
+    return {
+      num: 2
+    }
+  }
+}
+</script>
