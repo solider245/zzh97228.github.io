c54ea723f48590f6b6339f51e9b515f97ab3fc6d
lagabu <1002723879@qq.com>
2020-06-26 23:44:44 +0800

    feat: replace docs with _assets

--- build.js
+++ build.js
@@ -2,6 +2,6 @@ const shell = require('shelljs');
 
 shell.rm('-rf', './index.html', './_assets', './.vitepress/dist', './dist', './docs');
 shell.exec('npx vitepress build');
-shell.mv('./.vitepress/dist', './docs');
+shell.mv('./.vitepress/dist/*', '.');
 
 // shell.rm('-rf', './.vitepress/dist')
